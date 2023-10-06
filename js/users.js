adaab76a9b90e33ac640c61cb17e2b087ea42637
Marianne Anctil <2163768@csfoy.ca>
2023-10-06 14:25:04 -0400

    affichage users terminer

--- js/users.js
+++ js/users.js
@@ -1,17 +1,39 @@
 
 
 
-function fetchInput(){
     fetch('https://randomuser.me/api?results=10')
         .then(response=>response.json())
+        .then(json=>data = json.results)
         .then(data=>afficherUsers(data));
-}
+
 
 function afficherUsers(data){
     const section=document.getElementById('liste');
-    console.log(data)
+    console.log(data);
 
-    // for(let cpt=0; cpt<data.length; cpt++){
-    //     console.log('info');
-    // }
+    let nouvListe = document.createElement('ul');
+    section.appendChild(nouvListe);
+    nouvListe.classList.add('liste');
+
+    for(let cpt=0; cpt<data.length; cpt++){
+        console.log('info');
+
+        let nouvItemListe = document.createElement('li');
+        nouvItemListe.classList.add('liste__item'+cpt);
+        nouvListe.appendChild(nouvItemListe);
+
+        let nouvBoutons = document.createElement('button');
+        nouvBoutons.setAttribute('data-index-user', cpt);
+        nouvItemListe.appendChild(nouvBoutons);
+
+        let imgUser = document.createElement('img');
+        imgUser.src=data[cpt].picture.large;
+        nouvBoutons.appendChild(imgUser);
+
+
+
+
+
+        console.log(section);
+    }
 }
