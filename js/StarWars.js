46140bce42aeb584fcc90c248a549820bb9d8597
Marianne Anctil <2163768@csfoy.ca>
2023-10-19 15:44:02 -0400

    css terminer + cleanup final

--- js/StarWars.js
+++ js/StarWars.js
@@ -1,200 +1,241 @@
-
 let arrayPlanetesSelect = [];
-let noImg=0;
+let noImg = 0;
 let intervale;
-let valeurVitesse=null;
+let valeurVitesse = null;
 let statutIntervale = true;
 
+
+/**
+ * Elements HTML
+ * @type {HTMLHeadingElement}
+ */
 let nomPLanete = document.createElement('h2');
-nomPLanete.innerHTML='';
+nomPLanete.innerHTML = '';
 document.getElementById('image').appendChild(nomPLanete);
 
+let nouvSpan = document.createElement('span');
+nouvSpan.classList.add('noDisplay');
+nouvSpan.classList.add('underline');
+document.getElementById('image').appendChild(nouvSpan);
+
 let nouvImg = document.createElement('img');
-nouvImg.src='';
-nouvImg.alt='';
+nouvImg.src = '';
+nouvImg.alt = '';
 document.getElementById('image').appendChild(nouvImg);
 
 
 let btnPrecedent = document.createElement('button');
-btnPrecedent.innerHTML='Précédent';
-btnPrecedent.id='btnPrecedent';
+btnPrecedent.innerHTML = 'Précédent';
+btnPrecedent.id = 'btnPrecedent';
 btnPrecedent.classList.add('noDisplay');
-document.getElementById('image').appendChild(btnPrecedent);
+document.getElementById('buttons').appendChild(btnPrecedent);
 
 let btnArret = document.createElement('button');
-btnArret.innerHTML='Arrêter';
-btnArret.id='btnArret';
+btnArret.innerHTML = 'Arrêter';
+btnArret.id = 'btnArret';
 btnArret.classList.add('noDisplay');
-document.getElementById('image').appendChild(btnArret);
+document.getElementById('buttons').appendChild(btnArret);
 
 let btnSuivant = document.createElement('button');
-btnSuivant.innerHTML='Suivant';
-btnSuivant.id='btnSuivant';
+btnSuivant.innerHTML = 'Suivant';
+btnSuivant.id = 'btnSuivant';
 btnSuivant.classList.add('noDisplay');
-document.getElementById('image').appendChild(btnSuivant);
-
+document.getElementById('buttons').appendChild(btnSuivant);
 
 
 let vitesseLent = document.createElement('option');
 vitesseLent.innerHTML = 'Lent';
-vitesseLent.value = '3000';
+vitesseLent.value = '1500';
 document.getElementById('vitesse').appendChild(vitesseLent);
 
 let vitesseMoyen = document.createElement('option');
 vitesseMoyen.innerHTML = 'Moyen';
-vitesseMoyen.value = '1500';
+vitesseMoyen.value = '1000';
 document.getElementById('vitesse').appendChild(vitesseMoyen);
 
 let vitesseRapide = document.createElement('option');
 vitesseRapide.innerHTML = 'Rapide';
-vitesseRapide.value = '700';
+vitesseRapide.value = '500';
 document.getElementById('vitesse').appendChild(vitesseRapide);
 
-
-
-
+/**
+ * Event listener
+ */
 document.getElementById('btnPrecedent').addEventListener('click', changerImage);
 document.getElementById('btnSuivant').addEventListener('click', changerImage);
 
-if(statutIntervale === true) {
+if (statutIntervale === true) {
     document.getElementById('btnArret').addEventListener('click', arreterVitesse);
-} else if(statutIntervale === false){
+} else if (statutIntervale === false) {
     document.getElementById('btnArret').addEventListener('click', demarrerVitesse);
 
 }
 
 
-
-
 fetch('https://swapi.dev/api/films/')
-    .then(response=>response.json())
-    // .then(json=>console.log(json));
-    .then(json=>data = json.results)
-    .then(data=>afficherFilms(data));
+    .then(response => response.json())
+    .then(json => data = json.results)
+    .then(data => afficherFilms(data));
 
 
+/**
+ * @param data
+ * afficher la liste des films et ajout d'un event change pour faire apparaitre
+ * les images et boutons lorsqu'un film est sélectionné
+ */
 
-
-
-
-function afficherFilms(data){
-    console.log(data)
+function afficherFilms(data) {
     const liste = document.getElementById('films');
 
-    for (cpt=0; cpt < 3; cpt++){
+    for (cpt = 0; cpt < 3; cpt++) {
         let nouvSelectFilm = document.createElement('option');
         nouvSelectFilm.innerHTML = data[cpt].title;
         nouvSelectFilm.setAttribute('value', cpt);
-        liste.appendChild(nouvSelectFilm)
+        liste.appendChild(nouvSelectFilm);
     }
 
-    liste.addEventListener('change', function(evenement)
-    {
-        arrayPlanetesSelect=[];
+    liste.addEventListener('change', function (evenement) {
+        arrayPlanetesSelect = [];
         document.querySelector('option[value=base]').classList.add('noDisplay');
         btnSuivant.classList.remove('noDisplay');
         btnArret.classList.remove('noDisplay');
         btnPrecedent.classList.remove('noDisplay');
+        nouvSpan.classList.remove('noDisplay');
         document.getElementById('vitesse').classList.remove('noDisplay');
         let objCible = evenement.currentTarget;
-        console.log(objCible.value)
         let arrayPlanetes = data[objCible.value].planets;
         fetchPlanetes(arrayPlanetes, objCible.value);
-        recupIntervale(3000);
+        recupIntervale(1500);
     });
 }
 
 
-function fetchPlanetes(array, id){
-    console.log(array);
-    console.log(id);
-    let test='';
-for(cpt=0; cpt<array.length;cpt++) {
-    fetch(array[cpt])
-        .then(response => response.json())
-        // .then(json=>console.log(json));
-        .then(json => data = json.name)
-        .then(data => afficherPlanetes(data));
-}
+/**
+ * @param array
+ * @param id
+ * Appel API pour fetch les planetes selon le films a partir d'un tableau.
+ * Le tableau contient les liens api de chaque planetes
+ */
+
+function fetchPlanetes(array, id) {
+    let test = '';
+    for (cpt = 0; cpt < array.length; cpt++) {
+        fetch(array[cpt])
+            .then(response => response.json())
+            .then(json => data = json.name)
+            .then(data => afficherPlanetes(data));
+    }
 }
 
-function afficherPlanetes(data){
-    arrayPlanetesSelect.push(data)
-console.log(arrayPlanetesSelect);
+/**
+ * @param data
+ * afficher la première planete trouver par l'API
+ */
+
+function afficherPlanetes(data) {
+    arrayPlanetesSelect.push(data);
     nouvImg.src = '../images/' + arrayPlanetesSelect[0] + '.jpeg';
-    nomPLanete.innerHTML=arrayPlanetesSelect[0];
-   console.log(nouvImg.src);
+    nomPLanete.innerHTML = 'Planète ' + arrayPlanetesSelect[0];
 
 }
 
-function changerImage(evenement){
+/**
+ * @param evenement
+ * selon le bouton presse, l'image va changer
+ */
+
+function changerImage(evenement) {
     let objCible = evenement.currentTarget;
 
-        if(objCible.id === 'btnSuivant'){
-            noImg++;
-            if(noImg > arrayPlanetesSelect.length-1){
-                noImg=0;
-            }
+    if (objCible.id === 'btnSuivant') {
+        noImg++;
+        if (statutIntervale === true && valeurVitesse !== null) {
+            clearInterval(intervale);
+            intervale = setInterval(modifierVitesse, valeurVitesse);
+        } else if (statutIntervale === true && valeurVitesse === null) {
+            clearInterval(intervale);
+            intervale = setInterval(modifierVitesse, 1500);
         }
-            if (objCible.id === 'btnPrecedent') {
-                noImg--;
-                if(noImg < 0) {
-                    noImg=arrayPlanetesSelect.length-1;
-            }
+        if (noImg > arrayPlanetesSelect.length - 1) {
+            noImg = 0;
         }
+    }
+    if (objCible.id === 'btnPrecedent') {
+        noImg--;
+        if (statutIntervale === true && valeurVitesse !== null) {
+            clearInterval(intervale);
+            intervale = setInterval(modifierVitesse, valeurVitesse);
+        } else if (statutIntervale === true && valeurVitesse === null) {
+            clearInterval(intervale);
+            intervale = setInterval(modifierVitesse, 1500);
+        }
+        if (noImg < 0) {
+            noImg = arrayPlanetesSelect.length - 1;
+        }
+    }
     nouvImg.src = '../images/' + arrayPlanetesSelect[noImg] + '.jpeg';
-    nomPLanete.innerHTML=arrayPlanetesSelect[noImg];
-    console.log(noImg)
+    nomPLanete.innerHTML = 'Planète ' + arrayPlanetesSelect[noImg];
 }
 
+/**
+ * @param vitesse
+ * recuperation de l'intervale et appel d'un event change pour pour envoyer la valeur
+ * de la vitesse selectionnee
+ */
 
-function recupIntervale(vitesse){
+
+function recupIntervale(vitesse) {
     intervale = setInterval(modifierVitesse, vitesse);
-    console.log('recup')
 
-    document.getElementById('vitesse').addEventListener('change',function(evenement){
-        console.log('recup change')
+    document.getElementById('vitesse').addEventListener('change', function (evenement) {
         clearInterval(intervale);
         let objCible = evenement.currentTarget;
-        valeurVitesse = objCible.value
+        valeurVitesse = objCible.value;
         intervale = setInterval(modifierVitesse, valeurVitesse);
     })
 }
 
-function modifierVitesse(){
-    console.log('modifier')
+/**
+ * modifier la vitesse selon celle qui a ete selecitonnee
+ */
+
+function modifierVitesse() {
     noImg++;
-    if(noImg > arrayPlanetesSelect.length-1){
-        noImg=0;
+    if (noImg > arrayPlanetesSelect.length - 1) {
+        noImg = 0;
     }
     nouvImg.src = '../images/' + arrayPlanetesSelect[noImg] + '.jpeg';
-    nomPLanete.innerHTML=arrayPlanetesSelect[noImg];
+    nomPLanete.innerHTML = 'Planète ' + arrayPlanetesSelect[noImg];
 }
 
-function demarrerVitesse(){
+/**
+ * fonction pour demarrer le deroulement automatique
+ */
+
+function demarrerVitesse() {
     document.getElementById('btnArret').removeEventListener('click', demarrerVitesse);
     document.getElementById('btnArret').addEventListener('click', arreterVitesse);
     document.getElementById('vitesse').classList.remove('noDisplay');
-    statutIntervale=true;
-    if(valeurVitesse===null){
-        recupIntervale(3000);
-    }else{
+    statutIntervale = true;
+    if (valeurVitesse === null) {
+        recupIntervale(1500);
+    } else {
         recupIntervale(valeurVitesse);
     }
 
 
-    btnArret.innerHTML='Arrêter';
-    console.log(valeurVitesse);
+    btnArret.innerHTML = 'Arrêter';
 }
 
-function arreterVitesse(){
+/**
+ * fonction pour supprimer l'intervale et arreter le deroulement automatique
+ */
+
+function arreterVitesse() {
     document.getElementById('btnArret').removeEventListener('click', arreterVitesse);
     document.getElementById('btnArret').addEventListener('click', demarrerVitesse);
     document.getElementById('vitesse').classList.add('noDisplay');
-    statutIntervale=false;
+    statutIntervale = false;
     clearInterval(intervale);
-    btnArret.innerHTML='Démarrer';
-    console.log(statutIntervale)
+    btnArret.innerHTML = 'Démarrer';
 }
-
-console.log(statutIntervale);
--- js/users.js
+++ js/users.js
@@ -1,26 +1,20 @@
+fetch('https://randomuser.me/api?results=10')
+    .then(response => response.json())
+    .then(json => data = json.results)
+    .then(data => afficherUsers(data));
 
 
-
-    fetch('https://randomuser.me/api?results=10')
-        .then(response=>response.json())
-        .then(json=>data = json.results)
-        // .then(json=>console.log(json))
-        .then(data=>afficherUsers(data));
-
-
-function afficherUsers(data){
-    const section=document.getElementById('liste');
-    console.log(data);
+function afficherUsers(data) {
+    const section = document.getElementById('liste');
 
     let nouvListe = document.createElement('ul');
     section.appendChild(nouvListe);
     nouvListe.classList.add('liste');
 
-    for(let cpt=0; cpt<data.length; cpt++){
-        console.log('info');
+    for (let cpt = 0; cpt < data.length; cpt++) {
 
         let nouvItemListe = document.createElement('li');
-        nouvItemListe.classList.add('liste__item'+cpt);
+        nouvItemListe.classList.add('liste__item' + cpt);
         nouvListe.appendChild(nouvItemListe);
 
         let nouvBoutons = document.createElement('button');
@@ -28,41 +22,29 @@ function afficherUsers(data){
         nouvItemListe.appendChild(nouvBoutons);
 
         let imgUser = document.createElement('img');
-        imgUser.src=data[cpt].picture.large;
-        imgUser.alt='afficher la fiche de ' + data[cpt].name.first +' ' +  data[cpt].name.last;
+        imgUser.src = data[cpt].picture.large;
+        imgUser.alt = 'afficher la fiche de ' + data[cpt].name.first + ' ' + data[cpt].name.last;
         nouvBoutons.appendChild(imgUser);
-        console.log(section);
     }
 
     let boutons = document.querySelectorAll('button');
     let idUser = boutons.id;
-    console.log(boutons.length);
 
     // boutons.addEventListener('click', afficherFiche())
 
-    for (cptBtn=0; cptBtn<boutons.length; cptBtn++){
-        console.log(cptBtn);
-        boutons[cptBtn].addEventListener('click', function(evenement){
+    for (cptBtn = 0; cptBtn < boutons.length; cptBtn++) {
+        boutons[cptBtn].addEventListener('click', function (evenement) {
             let objCible = evenement.currentTarget;
-            let listeInfo = document.querySelectorAll('#fiche>ul>li')
+            let listeInfo = document.querySelectorAll('#fiche>ul>li');
 
             document.querySelector('#fiche>img').src = data[objCible.id].picture.large;
             document.querySelector('#fiche>img').alt = 'photo de ' + data[objCible.id].name.first + ' ' + data[objCible.id].name.last;
             document.querySelector('h2').innerHTML = data[objCible.id].name.first + ' ' + data[objCible.id].name.last;
-           listeInfo[0].innerHTML = data[objCible.id].location.street.number + ' ' + data[objCible.id].location.street.name;
-           listeInfo[1].innerHTML = data[objCible.id].location.city;
-           listeInfo[2].innerHTML = data[objCible.id].location.state;
-           listeInfo[3].innerHTML = data[objCible.id].location.postcode;
+            listeInfo[0].innerHTML = data[objCible.id].location.street.number + ' ' + data[objCible.id].location.street.name;
+            listeInfo[1].innerHTML = data[objCible.id].location.city;
+            listeInfo[2].innerHTML = data[objCible.id].location.state;
+            listeInfo[3].innerHTML = data[objCible.id].location.postcode;
 
         })
     }
 }
-
-
-// function afficherFiche(evenement){
-//     let objCible = evenement.currentTarget;
-//     console.log(objCible.id);
-//
-//     // document.querySelector('h2').innerHTML =
-//
-// }
